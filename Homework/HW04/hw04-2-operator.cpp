9f77126e28ca4c6f6d328efd27eba8c2beece66f
RubyQWER <Chanatpong1234@gmail.com>
2023-08-14 03:16:57 +0700

    fsdfs

--- Homework/HW04/hw04-2-operator.cpp
+++ Homework/HW04/hw04-2-operator.cpp
@@ -23,8 +23,8 @@ int main() {
     printf( "a -= b %d \n", a -= b ) ;
     printf( "a *= 1 %d \n", a*= 1 ) ;
     printf( "a *= b %d \n", a*= b ) ;
-    printf( "a %= 1 %d \n", a %= 1 ) ;
-    printf( "a %= b %d \n", a %= b ) ;
+    printf( "a %%= 1 %d \n", a %= 1 ) ;
+    printf( "a %%= b %d \n", a %= b ) ;
     printf( "a && b %d \n", a && b ) ;
     printf( "a || b %d \n", a || b ) ;
     return 0 ;
